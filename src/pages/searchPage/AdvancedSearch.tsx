f4c637316e7c6d35be850c9be9fd6edd74f934d5
aondrej ryan <aondrejryan11@gmail.com>
2024-10-12 12:48:34 -0700

    still working on advanced search

--- src/pages/searchPage/AdvancedSearch.tsx
+++ src/pages/searchPage/AdvancedSearch.tsx
@@ -6,20 +6,27 @@ function AdvancedSearch(this: any) {
     const [maxPro, setMaxPro] = useState<number | undefined>(undefined)
     const [maxFat, setMaxFat] = useState<number | undefined>(undefined)
     const [prep, setPrep] = useState<number | undefined>(undefined)
-    const [ingridents, setIngridents] = useState<string[]>([])
-    const [ingridentsEx, setIngridentsEx] = useState<string[]>([])
+    const [ingredients, setIngredients] = useState<string[]>([])
+    const [ingredientText, setIngredientText] = useState<string>('')
+    const [ingredientTextEx, setIngredientTextEx] = useState<string>('')
+    const [ingredientsEx, setIngredientsEx] = useState<string[]>([])
     
     interface SearchData{
-       maxCal:undefined | number, 
-       maxCarb:undefined | number, 
-       maxPro:undefined | number, 
-       maxFat:undefined | number, 
+        maxCal:undefined | number, 
+        maxCarb:undefined | number, 
+        maxPro:undefined | number, 
+        maxFat:undefined | number, 
+        ingredients: string[],
+        ingredientsEx: string[],
+        
     }
     const searchData:SearchData= {
         maxCal: maxCal, 
         maxCarb: maxCarb, 
         maxPro: maxPro, 
         maxFat: maxFat, 
+        ingredients:ingredients,
+        ingredientsEx: ingredientsEx
 
     }
     const handleCal = (e:any) => { 
@@ -34,7 +41,27 @@ function AdvancedSearch(this: any) {
     const handleCarb = (e:any) => { 
         Number(e.target.value) === 0 ? setMaxCarb(undefined) : setMaxCarb(Number(e.target.value))
     }
+    
+    function addOrExcludeIngredient(e:any, ingredientsExOrIn: string[], ingredientText: string, setIngredientsExOrIn: React.Dispatch<React.SetStateAction<any[]>>, checkIfInExOrIncludeList:string[], action:string){
+        e.preventDefault()
+        // checking to see if user was trying to include soemthing they excluded in a search or vic versa
+        if(checkIfInExOrIncludeList.includes(ingredientText)){
+            let pastAction: undefined | string = undefined
+            action === 'include' ? pastAction = 'exclude' : pastAction = 'include'
+            let confirmStatement = `You've already added ${ingredientText} to your ${pastAction} list! do you want now ${action} it?`
+            confirm(confirmStatement) ? null : null
+            if(confirm(confirmStatement)){
+                // i have to add parameter for to delete the text from an array
+                // i should have everything i need to then add it to the next array
+            } else {
+                return
+            }
+        }
 
+        const updatedIngredients = [...ingredientsExOrIn, ingredientText];
+        setIngredientsExOrIn([...new Set(updatedIngredients)]);
+    }
+    console.log(ingredients, ingredientsEx)
     useEffect(() => {
         console.log(maxCal)
         console.log(searchData)
@@ -66,8 +93,8 @@ function AdvancedSearch(this: any) {
                     <h2>ingredients included</h2>
                     <div>
                         <label style={{color:'black'}} htmlFor="">ingredients</label>
-                        <input type="text" />
-                        <button>add to search</button>
+                        <input type="text" onChange={(e:any) => setIngredientText(e.target.value)}/>
+                        <button onClick={(e) => addOrExcludeIngredient(e,ingredients,ingredientText,setIngredients, ingredientsEx, 'include' )}>add to search</button>
                     </div>
                </div>
 
@@ -75,8 +102,8 @@ function AdvancedSearch(this: any) {
                     <h2>ingredients excluded</h2>
                     <div>
                         <label style={{color:'black'}} htmlFor="">ingredients</label>
-                        <input type="text" />
-                        <button>add to search</button>
+                        <input type="text"  onChange={(e:any) => setIngredientTextEx(e.target.value)}/>
+                        <button onClick={(e) => addOrExcludeIngredient(e, ingredientsEx, ingredientTextEx, setIngredientsEx, ingredients, 'exclude')}>Exclude From Search</button>
                     </div>
                </div>
 
